f532932b1f790a44e1154238bda3006ace71634c
sawanthrishi <hrishikeshsawant10@gmail.com>
2019-06-08 16:05:14 +0530

    IC

---
+++ FrontEnd/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import render_template,url_for,flash,redirect,request,abort,Flask,jsonify
+
+app=Flask(__name__)
+
+@app.route('/home')
+def home():
+	return render_template('home.html')
+
+@app.route('/home',methods=['POST'])
+def data():
+	text=request.form['text']
+	f = open("../app/data/test.txt", "a")
+	f.write(text+"\n")
+	f.close()
+	return text
+'''
+def hello_world():
+	f = open("../app/data/test.txt", "w")
+	f.write("Woops! I have deleted the content!")
+	f.close()
+	return 'Flask Dockerized'
+'''
+if __name__=='__main__':
+	app.run(debug=True,host='0.0.0.0')
